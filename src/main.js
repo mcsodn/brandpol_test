73cca97eb4d2dc01dfb80a8eb4841afc3bdd4709
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-10-27 23:34:42 +0500

    add tailwind

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
-import "./style.css";
+import "./assets/styles/style.css";
 import App from "./App.vue";
 
 const app = createApp(App);
@@ -9,6 +9,12 @@ const app = createApp(App);
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
+    options: {
+      cssLayer: {
+        name: "primevue",
+        order: "theme, base, primevue",
+      },
+    },
   },
 });
 
