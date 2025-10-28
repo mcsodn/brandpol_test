3179531b9f757b0699c40b4767dc96447f12cfee
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-10-28 11:09:37 +0500

    add basic struct

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,59 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const selectedCity = ref();
+const cities = ref([
+  { name: "New York", code: "NY" },
+  { name: "Rome", code: "RM" },
+  { name: "London", code: "LDN" },
+  { name: "Istanbul", code: "IST" },
+  { name: "Paris", code: "PRS" },
+]);
+</script>
 
 <template>
-  <div class="">
-    <h1 class="">Привет, Мир!</h1>
+  <div class="mt-12 flex w-full flex-col items-center gap-4">
+    <div class="section-wrapper gap-4">
+      <Select
+        v-model="selectedCity"
+        :options="cities"
+        optionLabel="name"
+        placeholder="Все фильмы вселенной Star Wars"
+        class="w-full md:w-1/4"
+      />
+      <Button
+        icon="pi pi-cog"
+        label="Настройки"
+        severity="contrast"
+        variant="text"
+      />
+    </div>
+    <div class="section-wrapper">
+      <Card class="w-1/4">
+        <template #title>Планеты</template>
+        <template #content>
+          <p class="m-0">Всего планет: 500</p>
+          <p class="m-0">Планет в фильме: 350</p>
+        </template>
+      </Card>
+    </div>
+    <div class="section-wrapper">
+      <Card class="w-full">
+        <template #title>Корабли</template>
+        <template #content>
+          <p class="m-0">Всего планет: 500</p>
+          <p class="m-0">Планет в фильме: 350</p>
+        </template>
+      </Card>
+    </div>
+    <div class="section-wrapper">
+      <Card class="w-full">
+        <template #title>Корабли по классам</template>
+        <template #content>
+          <p class="m-0">Всего планет: 500</p>
+          <p class="m-0">Планет в фильме: 350</p>
+        </template>
+      </Card>
+    </div>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from "vue";
 import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
+import "primeicons/primeicons.css";
 import "./assets/styles/style.css";
 import App from "./App.vue";
+
+import Select from "primevue/select";
+import Button from "primevue/button";
+import Card from "primevue/card";
 
 const app = createApp(App);
 
@@ -10,12 +15,15 @@ app.use(PrimeVue, {
   theme: {
     preset: Aura,
     options: {
-      cssLayer: {
-        name: "primevue",
-        order: "theme, base, primevue",
-      },
+      prefix: "p",
+      darkModeSelector: "light",
+      cssLayer: false,
     },
   },
 });
+
+app.component("Select", Select);
+app.component("Button", Button);
+app.component("Card", Card);
 
 app.mount("#app");
