9c34d930fa1045a5bbf17c4c87fe2c98ad680aee
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-10-30 18:07:30 +0500

    add starship-class chart

---
+++ src/components/StarshipsClassStatistics.vue
@@ -0,0 +1,111 @@
+<script setup>
+import { computed } from "vue";
+import { useStarWarsStore } from "../stores/starWarsStore";
+
+const store = useStarWarsStore();
+
+const chartOptions = computed(() => {
+  if (!store.starshipsTypes?.total?.length) {
+    return [{}];
+  }
+  const shipCategories = !store.starshipsTypes.inFilm
+    ? store.starshipsTypes.total.map((v) => v.name)
+    : store.starshipsTypes.inFilm.map((v) => v.name);
+  const shipCounts = !store.starshipsTypes.inFilm
+    ? store.starshipsTypes.total.map((v) => v.data[0])
+    : store.starshipsTypes.inFilm.map((v) => v.data[0]);
+
+  return [
+    {
+      chart: {
+        type: "bar",
+
+        style: {
+          fontFamily: ["MuseoSansCyrl", "Arial", "sans - serif"],
+        },
+        backgroundColor: "transparent",
+        plotBackgroundColor: "transparent",
+        plotBorderWidth: 0,
+        plotShadow: false,
+
+        marginRight: 50,
+        borderWidth: 0,
+      },
+      title: {
+        text: "",
+      },
+      xAxis: {
+        categories: shipCategories,
+        lineWidth: 0,
+        labels: {
+          style: {
+            color: "#121216",
+            fontSize: "14px",
+            fontFamily: "MuseoSansCyrl, Arial, sans-serif",
+          },
+        },
+      },
+      yAxis: {
+        max: shipCounts[0],
+        allowDecimals: false,
+        title: "",
+      },
+      credits: {
+        enabled: false,
+      },
+      legend: {
+        enabled: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
+      plotOptions: {
+        series: {
+          color: "#3379E2",
+          borderRadius: {
+            radius: "50%",
+          },
+          pointWidth: 12,
+
+          dataLabels: {
+            enabled: true,
+            style: {
+              color: "#121216",
+              fontSize: "14px",
+              textShadow: false,
+              textOutline: "none",
+            },
+            verticalAlign: "middle",
+            align: "right",
+            crop: false,
+            overflow: "none",
+            x: 25,
+          },
+        },
+      },
+      series: [
+        {
+          name: "Ships",
+          data: shipCounts.slice(0, 11),
+        },
+      ],
+    },
+  ];
+});
+</script>
+
+<template>
+  <div class="section-wrapper">
+    <Card class="card-style" style="box-shadow: 2px 2px 14px 0px #01174f17">
+      <template #content>
+        <div class="card-content-wrapper">
+          <h2 class="chart-card-title">Корабли по классам</h2>
+          <highcharts
+            class="rounded-xl"
+            :options="chartOptions[0]"
+          ></highcharts>
+        </div>
+      </template>
+    </Card>
+  </div>
+</template>
